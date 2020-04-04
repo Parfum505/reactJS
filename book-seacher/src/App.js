6df341182f210a102d2c105976a3568a7f32bdd3
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-04 14:46:37 +0200

    start a new project Book searcher

---
+++ book-seacher/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="bookSearcher">
+          <p>Text</p>
+        </div>
+    )
+  }
+}
+
+export default App;
